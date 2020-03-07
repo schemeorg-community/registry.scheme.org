a2a4c9624c5aa6c00943642f606c59e579b5cf0b
Lassi Kortela <lassi@lassi.io>
2020-03-07 17:07:52 +0200

    Add scheme-id file

---
+++ scheme-id.scm
@@ -0,0 +1,53 @@
+;;; Scheme implementation identifiers
+
+(id bigloo)
+(title "Bigloo")
+(contact "Manuel Serrano")
+
+(id chezscheme)
+(title "Chez Scheme")
+(contact "Kent Dybvig")
+
+(id chibi)
+(title "Chibi-Scheme")
+(contact "Alex Shinn")
+
+(id chicken)
+(title "CHICKEN")
+(contact "Felix Winkelmann")
+
+(id cyclone)
+(title "Cyclone Scheme->C compiler")
+(contact "Justin Ethier")
+
+(id gambit)
+(title "Gambit Scheme")
+(contact "Marc Feeley")
+
+(id gauche)
+(title "Gauche Scheme")
+(contact "Shiro Kawai")
+
+(id gerbil)
+(title "Gerbil Scheme")
+(contact "Dimitris Vyzovitis")
+
+(id guile)
+(title "GNU Guile")
+(contact "Andy Wingo")
+
+(id kawa)
+(title "Kawa Scheme")
+(contact "Per Bothner")
+
+(id larceny)
+(title "Larceny")
+(contact "William Clinger")
+
+(id sagittarius)
+(title "Sagittarius Scheme")
+(contact "Takashi Kato")
+
+(id stklos)
+(title "STklos")
+(contact "Erick Gallesio")

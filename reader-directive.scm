dd6f9f7c97934eba0f731299b935e5d424ff652a
Lassi Kortela <lassi@lassi.io>
2020-07-31 17:21:50 +0300

    Add SRFI 89 reader directives

--- reader-directive.scm
+++ reader-directive.scm
@@ -10,9 +10,17 @@
 (purpose "change to case insensitive identifiers")
 (rnrs r7rs)
 
+(directive "#!key")
+(purpose "start keyword arguments in lambda list")
+(srfi 89)
+
 (directive "#!no-fold-case")
 (purpose "change to case sensitive identifiers")
 (rnrs r7rs)
+
+(directive "#!optional")
+(purpose "start optional arguments in lambda list")
+(srfi 89)
 
 (directive "#!r6rs")
 (purpose "change to R6RS read syntax")
@@ -21,3 +29,7 @@
 (directive "#!r7rs")
 (purpose "change to R7RS read syntax")
 (implementations gauche)
+
+(directive "#!rest")
+(purpose "start rest argument in lambda list")
+(srfi 89)

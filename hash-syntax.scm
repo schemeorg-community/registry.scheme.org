d1a7d54696108d0e54d7232204c9519ad2497398
Lassi Kortela <lassi@lassi.io>
2020-08-06 23:46:55 +0300

    Add hash-syntax data file

---
+++ hash-syntax.scm
@@ -0,0 +1,41 @@
+(id "#!")
+(description "Identifier follows [R6RS+]; Unix script [SRFI 22]")
+
+(id "#\"")
+(description "Interpolated string [Gauche]")
+
+(id "#(")
+(description "Vector [R7RS]")
+
+(id "#*")
+(description "Incomplete string [Gauche]")
+
+(id "#,")
+(description "Reader constructor [SRFI 10]")
+
+(id "#/")
+(description "Regular expression [Gauche]")
+
+(id "#:")
+(description "Uninterned symbol [Gauche]")
+
+(id "#;")
+(description "S-expression comment [SRFI 62]")
+
+(id "#<")
+(description "Unreadable object [Gauche]")
+
+(id "#?")
+(description "Debug macro [Gauche]")
+
+(id "#[")
+(description "Character set [Gauche]")
+
+(id "#\\")
+(description "Literal character [R7RS]")
+
+(id "#`")
+(description "String interpolation (legacy) [Gauche]")
+
+(id "#|")
+(description "Block comment until |# [SRFI 30]")

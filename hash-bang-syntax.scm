3042f750cabd802178ee7b1c6b768a796a11a720
Lassi Kortela <lassi@lassi.io>
2020-08-07 10:53:02 +0300

    Say where each #! identifier came from

--- hash-bang-syntax.scm
+++ hash-bang-syntax.scm
@@ -1,26 +1,26 @@
 (id bwp)
-(description "broken-weak-pair object")
+(description "broken-weak-pair object [Chez Scheme]")
 
 (id eof)
 (description "end-of-file object")
 
 (id fold-case)
-(description "change to case insensitive identifiers")
+(description "change to case insensitive identifiers [R7RS]")
 
 (id key)
-(description "start keyword arguments in lambda list")
+(description "start keyword arguments in lambda list [DSSSL]")
 
 (id no-fold-case)
-(description "change to case sensitive identifiers")
+(description "change to case sensitive identifiers [R7RS]")
 
 (id optional)
-(description "start optional arguments in lambda list")
+(description "start optional arguments in lambda list [DSSSL]")
 
 (id r6rs)
-(description "change to R6RS read syntax")
+(description "change to R6RS read syntax [R6RS]")
 
 (id r7rs)
 (description "change to R7RS read syntax")
 
 (id rest)
-(description "start rest argument in lambda list")
+(description "start rest argument in lambda list [DSSSL]")

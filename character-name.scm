7efd5b41226305087dddfacc322987e71d9d6f2a
Lassi Kortela <lassi@lassi.io>
2020-08-17 09:40:39 +0300

    Give character-name Unicode codepoints

--- character-name.scm
+++ character-name.scm
@@ -1,41 +1,41 @@
 (id alarm)
-(description "[R6RS, R7RS]")
+(description "U+0007 [R6RS, R7RS]")
 
 (id backspace)
-(description "[R6RS, R7RS]")
+(description "U+0008 [R6RS, R7RS]")
 
 (id delete)
-(description "[R6RS, R7RS]")
+(description "U+007F [R6RS, R7RS]")
 
 (id esc)
-(description "[R6RS]")
+(description "U+001B [R6RS]")
 
 (id escape)
-(description "[R7RS]")
+(description "U+001B [R7RS]")
 
 (id linefeed)
-(description "[R6RS]")
+(description "U+000A [R6RS]")
 
 (id newline)
-(description "[R6RS, R7RS]")
+(description "U+000A [R6RS, R7RS]")
 
 (id nul)
-(description "[R6RS]")
+(description "U+0000 [R6RS]")
 
 (id null)
-(description "[R7RS]")
+(description "U+0000 [R7RS]")
 
 (id page)
-(description "[R6RS]")
+(description "U+000C [R6RS]")
 
 (id return)
-(description "[R6RS, R7RS]")
+(description "U+000D [R6RS, R7RS]")
 
 (id space)
-(description "[R6RS, R7RS]")
+(description "U+0020 [R6RS, R7RS]")
 
 (id tab)
-(description "[R6RS, R7RS]")
+(description "U+0009 [R6RS, R7RS]")
 
 (id vtab)
-(description "[R6RS]")
+(description "U+000B [R6RS]")

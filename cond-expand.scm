552a2276f1fb8e4c63e699b6c8a058388e234cc1
Lassi Kortela <lassi@lassi.io>
2021-07-20 17:21:55 +0300

    Add cond-expand registry file

---
+++ cond-expand.scm
@@ -0,0 +1,19 @@
+(id and)
+(args expr ...)
+(description "True iif every expr is true. [SRFI 0]")
+
+(id not)
+(args expr)
+(description "True iif expr is false. [SRFI 0]")
+
+(id or)
+(args expr ...)
+(description "True iif at least one expr is true. [SRFI 0]")
+
+(id library)
+(args name)
+(description "True iif library name is importable. [R7RS]")
+
+(id compilation-target)
+(args ...)
+(description "[Gambit]")

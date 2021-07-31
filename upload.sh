87c84639a13be6bcac6f58f749ee081b697eb726
Lassi Kortela <lassi@lassi.io>
2021-07-31 14:06:14 +0300

    Update rsync flags

--- upload.sh
+++ upload.sh
@@ -2,4 +2,4 @@
 set -eu
 cd "$(dirname "$0")"
 gosh generate.scm
-rsync index.html alpha.servers.scheme.org:/production/registry/www/
+rsync -crv index.html alpha.servers.scheme.org:/production/registry/www/

464d84c913b4ff597be3faa787a5e9374b57dfbd
Lassi Kortela <lassi@lassi.io>
2023-12-27 14:27:08 +0200

    Upload to new server

--- upload.sh
+++ upload.sh
@@ -2,4 +2,4 @@
 set -eu
 cd "$(dirname "$0")"
 gosh generate.scm
-rsync -crv index.html *.pose alpha.servers.scheme.org:/production/registry/www/
+rsync -crv index.html *.pose tuonela.scheme.org:/production/registry/www/
